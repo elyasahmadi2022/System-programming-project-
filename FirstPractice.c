ecb7fc7ffdae90cb4868232ef12a3e5cc16f49ef
elyasahmadi2022 <154730091+elyasahmadi2022@users.noreply.github.com>
2024-02-22 10:08:55 +0430

    Update FirstPractice.c
    just using the printf and scanf

--- FirstPractice.c
+++ FirstPractice.c
@@ -2,5 +2,10 @@
 int main(){
     printf("Aslam Alikum ! Welcome to our First practice about our project");
     printf("I will try my best to solve this project as well as possible.");
-    printf("Hello our nice frinds" )
+    printf("Hello our nice frinds" );
+    char name[100];
+    printf("Enter Your name please");
+    scanf("%s", &name);
+
+    
 }
