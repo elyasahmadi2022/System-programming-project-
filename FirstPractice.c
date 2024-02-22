556968589493d52774fee992c840d2fbc44f2f40
Murtazamirzad <154867360+Murtazamirzad@users.noreply.github.com>
2024-02-21 21:30:33 -0800

    Update FirstPractice.c
    it is a simple code for starting it

--- FirstPractice.c
+++ FirstPractice.c
@@ -2,4 +2,5 @@
 int main(){
     printf("Aslam Alikum ! Welcome to our First practice about our project");
     printf("I will try my best to solve this project as well as possible.");
+    printf("Hello our nice frinds" )
 }
