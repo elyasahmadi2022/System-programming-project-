a5a710ba1bb3a0b135dd8041890bc5d8d50c134e
elyasahmadi2022 <154730091+elyasahmadi2022@users.noreply.github.com>
2024-02-26 21:01:30 +0430

    Update FirstPractice.c

--- FirstPractice.c
+++ FirstPractice.c
@@ -2,11 +2,5 @@
 int main(){
     printf("Aslam Alikum ! Welcome to our First practice about our project");
     printf("I will try my best to solve this project as well as possible.");
-    printf("Hello our nice frinds" );
-    char name[20];
-    printf("Enter Your name please");
-    scanf("%s", &name);
-    printf("our project is about file system map");
-    printf("We can start work on project when we find some information about it.");
-    printf("In this week I had some problem and I could not worked on the project.");
+  
 }
