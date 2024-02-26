efa537bee579e113f95ec3964b91703d00149980
elyasahmadi2022 <154730091+elyasahmadi2022@users.noreply.github.com>
2024-02-26 11:55:23 -0500

    About writing ShellScript
    There are some important thing that we should take care of them:
    echo --> which is a function Like printf() in C programming Language that will prints String in the screen.
    $BASH --> which is Variable Like we have in C or C++ programming Language that store a value.
    There we have Different Kind of Variable that is called System Variable

---
+++ File_System_Map.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+echo"Welcome For the First Line of Code in Shell Scripts"
+echo My BASH Name is $BASH # This a variable that will shows the Bash Name in Linux
+echo My Bash Version is $BASH_VERSION #This is a variable that will shows the Bash Version
+echo My Home Directory is $HOME # This is a variable that will shows the Home directory in Linux.
