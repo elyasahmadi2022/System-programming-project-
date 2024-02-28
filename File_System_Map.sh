6cafefe186823a3aad0e224518bfb5e96bb5d237
AliYahyaAlizada <154955919+AliYahyaAlizada@users.noreply.github.com>
2024-02-28 10:43:29 -0800

    Update File_System_Map.sh

--- File_System_Map.sh
+++ File_System_Map.sh
@@ -3,3 +3,4 @@ echo"Welcome For the First Line of Code in Shell Scripts"
 echo My BASH Name is $BASH # This a variable that will shows the Bash Name in Linux
 echo My Bash Version is $BASH_VERSION #This is a variable that will shows the Bash Version
 echo My Home Directory is $HOME # This is a variable that will shows the Home directory in Linux.
+echo "If we want to make a file system map first we should make a directoty"
