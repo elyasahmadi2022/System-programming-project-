83debc30c99e3d9fa19f85909edfee8c7b7d5488
elyasahmadi2022 <154730091+elyasahmadi2022@users.noreply.github.com>
2024-03-01 12:39:24 +0430

    Update File_System_Map.sh
    reading input from Keyboard

--- File_System_Map.sh
+++ File_System_Map.sh
@@ -4,3 +4,10 @@ echo My BASH Name is $BASH # This a variable that will shows the Bash Name in Li
 echo My Bash Version is $BASH_VERSION #This is a variable that will shows the Bash Version
 echo My Home Directory is $HOME # This is a variable that will shows the Home directory in Linux.
 echo "If we want to make a file system map first we should make a directoty"
+#here we are going to get input from keyboard 
+echo "Hello , please Enter Your name"
+read $name
+echo "Your Name is $name"
+echo "How old are you "
+read $age
+echo "You are about $age year old"
