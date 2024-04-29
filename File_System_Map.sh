57a28da7740ef23e2f1e3f0485117d736182b8b5
elyasahmadi2022 <elyasahmadiiran2022@gmail.com>
2024-04-29 06:40:02 -0500

    This completely something new

--- File_System_Map.sh
+++ File_System_Map.sh
@@ -1,26 +1,15 @@
-#!/usr/bin/bash
-echo"Welcome For the First Line of Code in Shell Scripts"
-echo My BASH Name is $BASH # This a variable that will shows the Bash Name in Linux
-echo My Bash Version is $BASH_VERSION #This is a variable that will shows the Bash Version
-echo My Home Directory is $HOME # This is a variable that will shows the Home directory in Linux.
-echo "If we want to make a file system map first we should make a directoty"
-#here we are going to get input from keyboard 
-echo "Hello , please Enter Your name"
-read $name
-echo "Your Name is $name"
-echo "How old are you "
-read $age
-echo "You are about $age years old"
+#!/bin/bash
+echo "Please Enter Name:"
+read name;
+echo "Please Enter Your Age:"
+read age;
+echo "Your Name: $name You are about: $age years old"
 
-if [ $# -eq 0 ]; then
-    echo "Usage: FileSystemMap <directory>"
-    exit 1
+echo"-----------------------------------------------------"
+if [ $age -gt 18 ]
+then 
+    echo "Mr/Miss $name you are eligiable to vote"
+else
+    echo "Mr/Miss $name you are not eligiable to vote"
 fi
-directory="$1"
 
-if [ ! -d "$directory" ]; then
-    echo "Directory does not exist: $directory"
-    exit 1
-fi
-find "$directory" -type d -print
-find "$directory" -type f -print
